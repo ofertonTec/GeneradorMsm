9bf883d33f61eb336a68d72fb013a9f8b6503317
Joel Llacsahuache Copia <joel.llacsahuache@upeu.edu.pe>
2022-09-22 08:11:19 -0500

    Mostrando y ocultando elementos

--- js/inicio.js
+++ js/inicio.js
@@ -1,26 +1,18 @@
 let options= document.getElementById('opciones')
 let btn= document.getElementById('boton')
-btn.addEventListener('click', verificarSelector)
 
 options.addEventListener('change', alertarCambio)
 function alertarCambio(){
-    
     let opcion= options.value
-    switch(opcion.toLowerCase){
-        case "campaña":
-            let monto= document.getElementById('monto')
-            monto.style.display="none"
+
+    switch(opcion.toLowerCase()){
+        case 'campaña' :
+            document.getElementById("monto_label").style.display="none"
+            document.getElementById("monto_input").style.display="none"
             break;
-        case "recordatorio":
-            document.getElementById("campaña").style.display="none";
-            document.getElementById("recordatorio").style.display="flex";
+        case 'recordatorio' :
+            document.getElementById("monto_label").style.display="flex"
+            document.getElementById("monto_input").style.display="flex"
             break;
     }
-}
-function verificarSelector(){
-    let opcion= options.value
-    let campaña= document.getElementById('campaña')
-    let recordatorio= document.getElementById('recordatorio')
-
-    
 }
